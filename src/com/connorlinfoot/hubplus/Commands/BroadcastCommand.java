54a4335b901be8a63c3b1dbc49de517f451e28d5
Connor Linfoot <connorlinfoot@me.com>
2014-06-13 15:33:19 +0100

    Version 0.5 DEV Build 8 - Moved Text Color into Global.UsefulFunctions

--- src/com/connorlinfoot/hubplus/Commands/BroadcastCommand.java
+++ src/com/connorlinfoot/hubplus/Commands/BroadcastCommand.java
@@ -1,6 +1,7 @@
 package com.connorlinfoot.hubplus.Commands;
 
 import com.connorlinfoot.hubplus.Global.Messages;
+import com.connorlinfoot.hubplus.Global.UsefulFunctions;
 import com.connorlinfoot.hubplus.HubPlus;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -28,7 +29,7 @@ public class BroadcastCommand implements CommandExecutor {
                             builder.append(value).append(" ");
                         }
                         String msg = builder.toString();
-                        msg = textColors(msg);
+                        msg = UsefulFunctions.textColors(msg);
                         instance.getServer().broadcastMessage(ChatColor.GOLD + instance.getConfig().getString("Broadcast Prefix") + " " + ChatColor.RESET + msg);
                         //player.sendMessage("");
                     }
@@ -42,28 +43,5 @@ public class BroadcastCommand implements CommandExecutor {
         return false;
     }
 
-    public String textColors( String msg ){
-        msg = msg.replace("&0", ChatColor.BLACK + "" );
-        msg = msg.replace("&1", ChatColor.DARK_BLUE + "" );
-        msg = msg.replace("&2", ChatColor.DARK_GREEN + "" );
-        msg = msg.replace("&3", ChatColor.DARK_AQUA + "" );
-        msg = msg.replace("&4", ChatColor.DARK_RED + "" );
-        msg = msg.replace("&5", ChatColor.DARK_PURPLE + "" );
-        msg = msg.replace("&6", ChatColor.GOLD + "" );
-        msg = msg.replace("&7", ChatColor.GRAY + "" );
-        msg = msg.replace("&8", ChatColor.DARK_GRAY + "" );
-        msg = msg.replace("&9", ChatColor.BLUE + "" );
-        msg = msg.replace("&a", ChatColor.GREEN + "" );
-        msg = msg.replace("&b", ChatColor.AQUA + "" );
-        msg = msg.replace("&c", ChatColor.RED + "" );
-        msg = msg.replace("&e", ChatColor.YELLOW + "" );
-        msg = msg.replace("&f", ChatColor.WHITE + "" );
-        msg = msg.replace("&k", ChatColor.MAGIC + "" );
-        msg = msg.replace("&l", ChatColor.BOLD + "" );
-        msg = msg.replace("&n", ChatColor.UNDERLINE + "" );
-        msg = msg.replace("&o", ChatColor.ITALIC + "" );
-        msg = msg.replace("&m", ChatColor.STRIKETHROUGH + "" );
-        msg = msg.replace("&r", ChatColor.RESET + "" );
-        return msg;
-    }
+    /* Moved Text Color to Global.UsefulFunctions */
 }
--- src/com/connorlinfoot/hubplus/Player/PlayerListener.java
+++ src/com/connorlinfoot/hubplus/Player/PlayerListener.java
@@ -1,5 +1,6 @@
 package com.connorlinfoot.hubplus.Player;
 
+import com.connorlinfoot.hubplus.Global.UsefulFunctions;
 import com.connorlinfoot.hubplus.HubPlus;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -62,7 +63,7 @@ public class PlayerListener implements Listener {
         Player player = event.getPlayer();
         String msg = instance.getConfig().getString("On Join Broadcast");
         String msgnew = msg.replace( "<playername>", player.getDisplayName() );
-        msgnew = textColors(msgnew);
+        msgnew = UsefulFunctions.textColors(msgnew);
         event.setJoinMessage(msgnew);
     }
 
@@ -249,30 +250,7 @@ public class PlayerListener implements Listener {
 
     /* Old clock function was here, No Longer Used! */
 
-    public String textColors( String msg ){
-        msg = msg.replace("&0", ChatColor.BLACK + "" );
-        msg = msg.replace("&1", ChatColor.DARK_BLUE + "" );
-        msg = msg.replace("&2", ChatColor.DARK_GREEN + "" );
-        msg = msg.replace("&3", ChatColor.DARK_AQUA + "" );
-        msg = msg.replace("&4", ChatColor.DARK_RED + "" );
-        msg = msg.replace("&5", ChatColor.DARK_PURPLE + "" );
-        msg = msg.replace("&6", ChatColor.GOLD + "" );
-        msg = msg.replace("&7", ChatColor.GRAY + "" );
-        msg = msg.replace("&8", ChatColor.DARK_GRAY + "" );
-        msg = msg.replace("&9", ChatColor.BLUE + "" );
-        msg = msg.replace("&a", ChatColor.GREEN + "" );
-        msg = msg.replace("&b", ChatColor.AQUA + "" );
-        msg = msg.replace("&c", ChatColor.RED + "" );
-        msg = msg.replace("&e", ChatColor.YELLOW + "" );
-        msg = msg.replace("&f", ChatColor.WHITE + "" );
-        msg = msg.replace("&k", ChatColor.MAGIC + "" );
-        msg = msg.replace("&l", ChatColor.BOLD + "" );
-        msg = msg.replace("&n", ChatColor.UNDERLINE + "" );
-        msg = msg.replace("&o", ChatColor.ITALIC + "" );
-        msg = msg.replace("&m", ChatColor.STRIKETHROUGH + "" );
-        msg = msg.replace("&r", ChatColor.RESET + "" );
-        return msg;
-    }
+    /* Moved Text Color to Global.UsefulFunctions */
 
     public class ClockCountdown implements Runnable {
         public Player player1 = null;
