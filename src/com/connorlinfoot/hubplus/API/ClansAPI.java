4ec64c4a206e0ef9b78e1b9e66de6988262b852b
Connor Linfoot <connorlinfoot@me.com>
2014-06-17 15:50:53 +0100

    Version 0.5 DEV Build 23 - Started work on a new API for other plugins (Including my future plugins) to access

---
+++ src/com/connorlinfoot/hubplus/API/ClansAPI.java
@@ -0,0 +1,138 @@
+package com.connorlinfoot.hubplus.API;
+
+import com.connorlinfoot.hubplus.Global.Messages;
+import com.connorlinfoot.hubplus.HubPlus;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.awt.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class ClansAPI extends JavaPlugin {
+
+    public static String getClan (Player player){
+        String uuid = String.valueOf(player.getUniqueId());
+        Statement statement = null;
+        try {
+            //statement = HubPlus.c.createStatement();
+            statement = HubPlus.getConnection().createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        ResultSet res;
+        Integer clanID = null;
+        try {
+            assert statement != null;
+            res = statement.executeQuery("SELECT * FROM HubPlus_Clans_Players WHERE uuid = '" + uuid + "';");
+            res.next();
+            if( res.getString("clanID") == null ){
+                clanID = 0;
+            } else {
+                clanID = res.getInt("clanID");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String clanName = null;
+        try {
+            res = statement.executeQuery("SELECT * FROM HubPlus_Clans WHERE clanID = '" + clanID + "';");
+            res.next();
+            if( res.getString("name") == null ){
+                clanName = "";
+            } else {
+                clanName = res.getString("name");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return clanName;
+    }
+
+    public static List getClanMembers(String name){
+        Plugin instance = HubPlus.getInstance();
+        Integer clanId = getClanID(name);
+        Statement statement = null;
+        try {
+            //statement = HubPlus.c.createStatement();
+            statement = HubPlus.getConnection().createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        ResultSet res;
+        List members = new List();
+        try {
+            assert statement != null;
+            res = statement.executeQuery("SELECT * FROM HubPlus_Clans_Players WHERE clanId = '" + clanId + "';");
+            Integer i = 0;
+            while (res.next()) {
+                if( i == 10 ) break;
+                i++;
+                UUID uuid = UUID.fromString(res.getString("UUID"));
+                members.add(instance.getServer().getOfflinePlayer(uuid).getName());
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return members;
+    }
+
+    public static Integer getClanID(String name){
+        //MySQL Statement to get clan ID
+        Statement statement = null;
+        try {
+            //statement = HubPlus.c.createStatement();
+            statement = HubPlus.getConnection().createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        ResultSet res;
+        Integer clanID = null;
+        try {
+            assert statement != null;
+            res = statement.executeQuery("SELECT * FROM HubPlus_Clans WHERE name = '" + name + "';");
+
+            res.next();
+            if( res.getString("clanID") == null ){
+                clanID = 0;
+            } else {
+                clanID = res.getInt("clanID");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return clanID;
+    }
+
+    public static boolean clanExists(String name){
+        Statement statement = null;
+        try {
+            statement = HubPlus.getConnection().createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        ResultSet res;
+        try {
+            res = statement.executeQuery("SELECT * FROM HubPlus_Clans WHERE name = '" + name + "';");
+            res.next();
+            if( res.getString("name") == null ){
+                return false;
+            } else {
+                return true;
+            }
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+}
